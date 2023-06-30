2fd9fdbe5e528e1e43cebe151eab3f7181966500
Damian Andaya <94016032+fdandaya@users.noreply.github.com>
2023-06-29 21:54:20 -0500

    Update forms.py

--- quotes/forms.py
+++ quotes/forms.py
@@ -3,10 +3,23 @@
 from django.db import models
 from django import forms
 from .models import User
+from .models import Quote
 
+class DateInput(forms.DateInput):
+        input_type = 'date'
 
 class CustomerForm(forms.ModelForm):
     class Meta:
         model = User
         fields = "__all__"
         #exclude = ['date_created','quote_amount']
+
+class FuelRequestForm(forms.ModelForm):
+    
+    class Meta:
+        model = Quote
+        fields = "__all__"
+        widgets = {
+            'delivery_date': DateInput(),
+        }
+        #exclude = ['date_created', 'quote_amount']
