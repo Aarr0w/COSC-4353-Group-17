ebdbf261cc0e3db8981947e213fdedbd277eb970
Damian Andaya <94016032+fdandaya@users.noreply.github.com>
2023-06-29 21:49:35 -0500

    Update urls.py

--- quotes/urls.py
+++ quotes/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name = 'home'),
     path('form/submit/', views.return_quote),   
-    path('register/', views.register),
+    path('register/', views.register, name = 'register'),
     path('profile/', views.register, name = 'profile'),
     path('fuel_request/', views.fuel_request, name = 'fuel_request'),
     path('login/', views.login_view, name='login')   
