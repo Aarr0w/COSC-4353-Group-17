218213c019f7f71b2c6e28a53a43bac4d12efad6
Damian Andaya <94016032+fdandaya@users.noreply.github.com>
2023-06-29 21:49:04 -0500

    Update urls.py

--- quotes/urls.py
+++ quotes/urls.py
@@ -3,8 +3,10 @@ from . import views
 
 #URLConf
 urlpatterns = [
-    path('home/', views.index, name = 'index'),
+    path('', views.index, name = 'home'),
     path('form/submit/', views.return_quote),   
     path('register/', views.register),
-    path('login/', views.login_view, name='login'),   
+    path('profile/', views.register, name = 'profile'),
+    path('fuel_request/', views.fuel_request, name = 'fuel_request'),
+    path('login/', views.login_view, name='login')   
 ]
