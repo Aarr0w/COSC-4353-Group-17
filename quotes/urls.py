5ad83575b691cdc5d0e1ca738704e2a8e9fb6f07
Github2287 <98567375+Github2287@users.noreply.github.com>
2023-06-30 13:42:06 -0500

    Login and Regiser here button correctly redirect

--- quotes/urls.py
+++ quotes/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name = 'home'),
     path('form/submit/', views.return_quote),   
-    path('profile', views.profile, name = 'profile'),
+    path('profile/', views.profile, name = 'profile'),
     path('fuel_request/', views.fuel_request, name = 'fuel_request'),
     path('login/', views.login_view, name='login'), 
     path('login/registration',views.login_register , name ='register' )  
