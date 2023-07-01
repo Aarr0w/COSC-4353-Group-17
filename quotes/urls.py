e6ecd2668d939d3e37d6e3b44cf04544cf29b978
Damian Andaya <94016032+fdandaya@users.noreply.github.com>
2023-06-30 20:42:02 -0500

    Added fuel history url

--- quotes/urls.py
+++ quotes/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('form/submit/', views.return_quote),   
     path('profile/', views.profile, name = 'profile'),
     path('fuel_request/', views.fuel_request, name = 'fuel_request'),
+    path('fuel_history/', views.fuel_history, name = 'fuel_history'),
     path('login/', views.login_view, name='login'), 
     path('login/registration',views.login_register , name ='register' )  
 ]
